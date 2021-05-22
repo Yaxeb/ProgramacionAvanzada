261570cb1c938d2f0f61cecdec2ca38d688bd9cc
azagraoscar <61020947+azagraoscar@users.noreply.github.com>
2021-05-22 18:41:27 +0200

    Desk documentation

--- PECL/src/pecl/Desk.java
+++ PECL/src/pecl/Desk.java
@@ -24,19 +24,33 @@ public class Desk {
     public void leaveWorker(){
         this.iDWorker = -1;
     }
-    
+    /**
+     * Sets the Patient ID on the desk
+     * @param idPatient The ID of the patient who is sitting on the desk
+     */
     public void setPatient(int idPatient){
         this.iDPatient = idPatient;
     }
-    
+    /**
+     * Returns the ID of the patient that is on the desk.
+     * If the number returned is -1, it means there is no patient on the desk
+     * @return The ID of the patient or -1 if there is no patient
+     */
     public int getPatient(){
         return this.iDPatient;
     }
-    
+    /**
+     * Sets the Worker ID on the desk
+     * @param idWorker The ID of the worker who is sitting on the desk
+     */
     public void setWorker(int idWorker){
         this.iDWorker = idWorker;
     }
-    
+    /**
+     * Returns the ID of the worker that is on the desk
+     * If the number returned is -1, it means there is no worker on the desk
+     * @return The ID of the worker or -1 if there is no worker
+     */
     public int getWorker(){
         return this.iDWorker;
     }

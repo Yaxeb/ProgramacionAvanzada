3451ce691b6dd2fe4d6d0eb7c73801f74327f0d9
azagraoscar <61020947+azagraoscar@users.noreply.github.com>
2021-05-23 22:27:36 +0200

    Update CustomLogger.java

--- PECL/src/pecl/CustomLogger.java
+++ PECL/src/pecl/CustomLogger.java
@@ -1,32 +1,14 @@
 package pecl;
 
 import java.io.BufferedWriter;
-import java.io.File;
 import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.logging.Level;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.concurrent.locks.*;
+
 public class CustomLogger {
-    private Lock locj = new ReentrantLock();
     private String path = "./Logs/EvolutionHospital.txt";
     private BufferedWriter bw;
     
-    //File path = new File("./Logs/EvolutionHospital.txt");
-    
-//    public synchronized void log(String message) {
-//        try {
-//            FileWriter myWriter = new FileWriter(path);
-//            myWriter.write(message);
-//            myWriter.close();
-//        } catch (IOException e) {
-//            System.out.println("An error occurred while logging.");
-//            e.printStackTrace();
-//        }
-//    }
-
     public synchronized void log(String message) {
         try {
             this.bw = new BufferedWriter(new FileWriter(path));
